3a5825b6c158d55fb3d7827118e3428893ebd0f2
Neo Pan <neopan@126.com>
2017-11-11 11:55:12 +0800

    remove eslint to global

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "standard"
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 8
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"]
+  }
 };
